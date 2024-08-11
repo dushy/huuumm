4813e97de090aaf134d60a1cb7fcd8e3cd7cefa7
Dushyant Hemant Dixit <dudixit@dudixit-mn7388.linkedin.biz>
2024-08-11 20:45:56 +0530

    First commit

---
+++ human.go
@@ -0,0 +1,12 @@
+package humanize
+
+import (
+	"fmt"
+
+	human "github.com/dustin/go-humanize"
+)
+
+func main() {
+	var number uint64 = 123456789
+	fmt.Println("Size of file is", human.Bytes(number))
+}
